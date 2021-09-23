1029f094f61ae6624a0cb910edd42d0a288af13b
Gabriel Speckhahn <749488+gabspeck@users.noreply.github.com>
2021-09-23 19:14:24 -0300

    highlight valid move (stub)

--- src/components/Board.svelte
+++ src/components/Board.svelte
@@ -1,8 +1,11 @@
-<canvas bind:this={canvas} width={width} height={height}></canvas>
+<canvas bind:this={canvas} on:mousemove={highlightValidMove} width={width} height={height}></canvas>
 <ul>
 	<li>Human: {score[Player.Human]}</li>
 	<li>Computer: {score[Player.Computer]}</li>
 </ul>
+<p>
+	<button on:click={reset}>Reset</button>
+</p>
 
 <script lang='ts'>
 
@@ -11,6 +14,28 @@
 	enum Player {
 		Computer = 'red',
 		Human = 'blue'
+	}
+
+	interface Point {
+		x: number,
+		y: number
+	}
+
+	enum Direction {
+		N = -1,
+		NE = 9,
+		E = 10,
+		SE = 11,
+		S = 1,
+		SW = -9,
+		W = -10,
+		NW = -11,
+	}
+
+	interface Move {
+		player: Player,
+		square: number
+		board: Player[]
 	}
 
 	const squareSide = 80;
@@ -22,14 +47,42 @@
 
 	let canvas: HTMLCanvasElement;
 	let ctx: CanvasRenderingContext2D;
-	let score: Record<Player, number> = {[Player.Human]: 0, [Player.Computer]: 0}
+	let score: Record<Player, number> = { [Player.Human]: 0, [Player.Computer]: 0 };
 
-	$: board.forEach((player: Player, xy: number) => {
-		const y = xy % 10;
-		const x = (xy - y) / 10;
+	$: board.forEach((player: Player, square: number) => {
 		score[player] += 1;
-		renderPiece(x, y, player);
-	})
+		renderPiece(square, player);
+	});
+
+	const pointToSquare = ({ x, y }: Point): number => {
+		const col = Math.trunc(x / squareSide + 1);
+		const row = Math.trunc(y / squareSide + 1);
+
+		return col * 10 + row;
+	};
+
+	const squareToPoint = (square: number): Point => {
+		const row = square % 10;
+		const col = (square - row) / 10;
+		const x = squareSide / 2 + ((col - 1) * squareSide);
+		const y = squareSide / 2 + ((row - 1) * squareSide);
+
+		return { x, y };
+	};
+
+	const isMoveValid = (move: Move): boolean => {
+		// stub
+		return move.square % 2 == 0;
+	};
+
+	const highlightValidMove = (ev: MouseEvent) => {
+		const { clientX, clientY } = ev;
+		const rect = (ev.target as Element).getBoundingClientRect();
+
+		const target = ev.target as HTMLElement;
+		const point: Point = { x: clientX - rect.left, y: clientY - rect.top };
+		target.style.cursor = isMoveValid({ player: Player.Human, square: pointToSquare(point), board }) ? 'crosshair' : 'not-allowed';
+	};
 
 	const renderGrid = () => {
 		let x = 0, y = squareSide;
@@ -49,9 +102,8 @@
 		ctx.stroke();
 	};
 
-	function renderPiece (c: number, r: number, color: string) {
-		let x = squareSide / 2 + ((c - 1) * squareSide);
-		let y = squareSide / 2 + ((r - 1) * squareSide);
+	function renderPiece(square: number, color: string) {
+		const { x, y } = squareToPoint(square);
 
 		ctx.beginPath();
 		ctx.arc(x, y, Math.floor(squareSide / 2), 0, 2 * Math.PI);
@@ -61,11 +113,14 @@
 
 	const reset = () => {
 		board = [];
+		for (let scoreKey in score) {
+			score[scoreKey] = 0;
+		}
 		board[44] = Player.Human;
 		board[55] = Player.Human;
 		board[45] = Player.Computer;
 		board[54] = Player.Computer;
-	}
+	};
 
 	onMount(() => {
 		ctx = canvas.getContext('2d');
